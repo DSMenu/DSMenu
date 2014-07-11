a2e05f3d84320c6b717ed99a230ab1aab7b0d200
Jonas Schnelli <jonasschnelli@Jonass-MacBook-Pro-3.local>
2014-07-11 16:36:44 +0200

    docs update (doxygen generated)

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['defaultmanager',['defaultManager',['../interface_m_d_d_s_s_manager.html#af0359d38979df1d414575dfb7aa6b537',1,'MDDSSManager']]],
+  ['defaultmanager',['defaultManager',['../interface_m_d_d_s_s_consumption_manager.html#ac7a7890115eff0ddaf1e4022d474f4bc',1,'MDDSSConsumptionManager::defaultManager()'],['../interface_m_d_d_s_s_manager.html#af0359d38979df1d414575dfb7aa6b537',1,'MDDSSManager::defaultManager()']]],
+  ['drawhistoryoncontext_3asize_3a',['drawHistoryOnContext:size:',['../interface_m_d_d_s_s_consumption_manager.html#ac4d5086c7cf8ce63e8e3996ba5e841ea',1,'MDDSSConsumptionManager']]],
   ['dsid',['dsid',['../interface_m_d_device_menu_item.html#aba1a1c831e4cace1935fef25e04a4b27',1,'MDDeviceMenuItem']]],
+  ['dsmnamefromid_3a',['dSMNameFromID:',['../interface_m_d_d_s_s_consumption_manager.html#a4347a6f14215e842e5629072c1a883c4',1,'MDDSSConsumptionManager']]],
   ['dssversionstring',['dSSVersionString',['../interface_m_d_d_s_s_manager.html#a49c80f7f7f2042658136095fe9f6928f',1,'MDDSSManager']]]
 ];
--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['callscene_3adeviceid_3acallback_3a',['callScene:deviceId:callback:',['../interface_m_d_d_s_s_manager.html#a33e9c999b7b7547d42c4102097102ee8',1,'MDDSSManager']]],
-  ['callscene_3azoneid_3agroupid_3acallback_3a',['callScene:zoneId:groupID:callback:',['../interface_m_d_d_s_s_manager.html#a5380dc94b95172a6131901f23a73bd7d',1,'MDDSSManager']]]
+  ['callscene_3azoneid_3agroupid_3acallback_3a',['callScene:zoneId:groupID:callback:',['../interface_m_d_d_s_s_manager.html#a5380dc94b95172a6131901f23a73bd7d',1,'MDDSSManager']]],
+  ['customscenenamesforgroup_3ainzone_3a',['customSceneNamesForGroup:inZone:',['../interface_m_d_d_s_s_manager.html#a879391644298f08b9dd399d69dc5b137',1,'MDDSSManager']]]
 ];
